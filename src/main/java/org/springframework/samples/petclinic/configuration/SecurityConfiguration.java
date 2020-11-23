d4db4ff6b6c29046f486e4db153c31661676e462
lauporsol <lauraportillosoler@gmail.com>
2020-11-23 10:13:33 +0100

    Implementación de H05

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -29,62 +29,42 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	DataSource dataSource;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/resources/**","/webjars/**","/h2-console/**").permitAll()
-				.antMatchers(HttpMethod.GET, "/","/oups").permitAll()
-				.antMatchers("/users/new").permitAll()
-				.antMatchers("/clientes").hasAnyAuthority("admin")
-				.antMatchers("/clientes/**").hasAnyAuthority("admin", "clientes")
-				.antMatchers("/clientes/**/editar").hasAnyAuthority("admin","clientes")
-				.antMatchers("/vendedores/**/editar").hasAnyAuthority("admin","vendedores")
-				.antMatchers("/vendedores/**/perfilCliente/**").hasAnyAuthority("admin", "vendedores")
-				.antMatchers("/solicitudes/**").hasAnyAuthority("admin")
-				.antMatchers("/bloqueos/**").hasAnyAuthority("admin")
-				.antMatchers("/moderadores/**").permitAll()
-				.antMatchers("/admin/**").hasAnyAuthority("admin")
-				.antMatchers("/owners/**").hasAnyAuthority("owner","admin")		
-				.antMatchers("/vendedores/**").hasAnyAuthority("admin", "vendedores")
-				.antMatchers("/vets/**").authenticated()
-				.anyRequest().denyAll()
-				.and()
-				 	.formLogin()
-				 	/*.loginPage("/login")*/
-				 	.failureUrl("/login-error")
-				.and()
-					.logout()
-						.logoutSuccessUrl("/"); 
-                // Configuración para que funcione la consola de administración 
-                // de la BD H2 (deshabilitar las cabeceras de protección contra
-                // ataques de tipo csrf y habilitar los framesets si su contenido
-                // se sirve desde esta misma página.
-                http.csrf().ignoringAntMatchers("/h2-console/**");
-                http.headers().frameOptions().sameOrigin();
+		http.authorizeRequests().antMatchers("/resources/**", "/webjars/**", "/h2-console/**").permitAll()
+				.antMatchers(HttpMethod.GET, "/", "/oups").permitAll().antMatchers("/users/new").permitAll()
+				.antMatchers("/clientes").hasAnyAuthority("admin").antMatchers("/clientes/**")
+				.hasAnyAuthority("admin", "clientes").antMatchers("/clientes/**/editar")
+				.hasAnyAuthority("admin", "clientes").antMatchers("/vendedores/**/editar")
+				.hasAnyAuthority("admin", "vendedores").antMatchers("/vendedores/**/perfilCliente/**")
+				.hasAnyAuthority("admin", "vendedores").antMatchers("/solicitudes/**").hasAnyAuthority("admin")
+				.antMatchers("/bloqueos/**").hasAnyAuthority("admin").antMatchers("/moderadores/**").permitAll()
+				.antMatchers("/admin/**").hasAnyAuthority("admin").antMatchers("/owners/**")
+				.hasAnyAuthority("owner", "admin").antMatchers("/vendedores/**").hasAnyAuthority("admin", "vendedores")
+				.antMatchers("/vets/**").authenticated().anyRequest().denyAll().and().formLogin()
+				/* .loginPage("/login") */
+				.failureUrl("/login-error").and().logout().logoutSuccessUrl("/");
+		// Configuración para que funcione la consola de administración
+		// de la BD H2 (deshabilitar las cabeceras de protección contra
+		// ataques de tipo csrf y habilitar los framesets si su contenido
+		// se sirve desde esta misma página.
+		http.csrf().ignoringAntMatchers("/h2-console/**");
+		http.headers().frameOptions().sameOrigin();
 	}
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication()
-	      .dataSource(dataSource)
-	      .usersByUsernameQuery(
-	       "select username,password,enabled "
-	        + "from users "
-	        + "where username = ?")
-	      .authoritiesByUsernameQuery(
-	       "select username, authority "
-	        + "from authorities "
-	        + "where username = ?")	      	      
-	      .passwordEncoder(passwordEncoder());	
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username,password,enabled " + "from users " + "where username = ?")
+				.authoritiesByUsernameQuery("select username, authority " + "from authorities " + "where username = ?")
+				.passwordEncoder(passwordEncoder());
 	}
-	
+
 	@Bean
-	public PasswordEncoder passwordEncoder() {	    
-		PasswordEncoder encoder =  NoOpPasswordEncoder.getInstance();
-	    return encoder;
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder encoder = NoOpPasswordEncoder.getInstance();
+		return encoder;
 	}
-	
+
 }
-
-
