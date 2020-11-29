17c5234577176809082d24affa909cb492818e1b
dansuaper <16dsp918@gmail.com>
2020-11-29 21:53:59 +0100

    Pruebas

---
+++ src/test/java/org/springframework/samples/petclinic/web/OfertaControllerTest.java
@@ -0,0 +1,76 @@
+package org.springframework.samples.petclinic.web;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.Oferta;
+import org.springframework.samples.petclinic.service.OfertaService;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers=OfertaController.class,
+excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
+excludeAutoConfiguration= SecurityConfiguration.class)
+
+public class OfertaControllerTest {
+	
+	private static final int TEST_OFERTA_ID = 1;
+
+	@Autowired(required=true)
+	private OfertaController ofertaController;
+	
+	@MockBean 
+	private OfertaService ofertaService;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	private Oferta oferta;
+	
+	@BeforeEach
+	void setup() {
+		oferta = new Oferta();
+		oferta.setId(TEST_OFERTA_ID);
+		oferta.setDisponibilidad(true);
+		oferta.setPorcentaje(10);
+		given(this.ofertaService.findOfertById(TEST_OFERTA_ID)).willReturn(oferta);
+	}
+	
+	@WithMockUser(value = "spring")
+    @Test
+    void testEdit() throws Exception {
+		mockMvc.perform(get("/vendedores/ofertas/"+TEST_OFERTA_ID)).andExpect(model().attributeExists("oferta"))
+		.andExpect(view().name("vendedores/editarOferta"));
+	}
+	
+	@WithMockUser(value = "spring")
+    @Test
+    void testProcesoOfertar() throws Exception{
+		mockMvc.perform(post("/vendedores/ofertas/"+TEST_OFERTA_ID).param("disponibilidad", "true")
+		.param("porcentaje", "10").with(csrf())).andExpect(status().is3xxRedirection())
+		.andExpect(view().name("redirect:/vendedores/articulosEnVenta"));
+	}
+	
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testProcesoDesofertar() throws Exception{
+		mockMvc.perform(get("/vendedores/ofertas/desofertar/"+TEST_OFERTA_ID)).andExpect(status().is3xxRedirection())
+		.andExpect(view().name("redirect:/vendedores/articulosEnVenta"));
+	}
+	
+}
