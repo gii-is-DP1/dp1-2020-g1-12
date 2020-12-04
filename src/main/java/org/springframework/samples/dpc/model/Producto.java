a4049817da5006ab85d9a7ad723de7b12db976bb
Fernando <ferherque@alum.us.es>
2020-12-04 21:19:34 +0100

    Añadido generos y productos relacionados dentro de la vista detalles.

--- src/main/java/org/springframework/samples/dpc/model/Producto.java
+++ src/main/java/org/springframework/samples/dpc/model/Producto.java
@@ -1,12 +1,23 @@
 package org.springframework.samples.dpc.model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ManyToMany;
 import javax.persistence.MappedSuperclass;
 
 import org.hibernate.validator.constraints.Length;
 
 @MappedSuperclass
 public class Producto extends BaseEntity {
+	
+	@ManyToMany(cascade =
+		{CascadeType.PERSIST,
+		CascadeType.MERGE,
+		CascadeType.DETACH,
+		CascadeType.REFRESH})
+		private Set<Genero> generos;
 	
 	@Column(name = "modelo")
 	@Length(min=3,max=200)
@@ -31,7 +42,14 @@ public class Producto extends BaseEntity {
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
-	
+
+	public Set<Genero> getGeneros() {
+		return generos;
+	}
+
+	public void setGeneros(Set<Genero> generos) {
+		this.generos = generos;
+	}
 	
 
 }
--- src/main/java/org/springframework/samples/dpc/service/ArticuloService.java
+++ src/main/java/org/springframework/samples/dpc/service/ArticuloService.java
@@ -1,10 +1,14 @@
 package org.springframework.samples.dpc.service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.dpc.model.Articulo;
+import org.springframework.samples.dpc.model.Genero;
 import org.springframework.samples.dpc.repository.ArticuloRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,6 +41,18 @@ public class ArticuloService {
 	@Transactional(readOnly = true)
 	public List<Articulo> articulosDisponibles() {
 		return articuloRepository.articulosDisponibles();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<Articulo> articulosRelacionados(Set<Genero> generos, int articuloId) {
+		List<Articulo> relacionados = new ArrayList<>();
+		List<Articulo> articulos = articuloRepository.articulosDisponibles();
+		articulos.sort(Comparator.comparing(Articulo::getId).reversed());
+		for(Articulo articulo:articulos) {
+			if(relacionados.size() < 6 && articulo.getId() != articuloId && articulo.getGeneros().containsAll(generos))
+				relacionados.add(articulo);
+		}
+		return relacionados;
 	}
 	
 }
