ac91695f326b01660bc20ff95763c7c880a9d93a
Jesus <jesusbarba11@gmail.com>
2020-12-05 13:51:27 +0100

    Añadido botón de dar de baja un articulo en vendedor

--- src/main/java/org/springframework/samples/dpc/service/ArticuloService.java
+++ src/main/java/org/springframework/samples/dpc/service/ArticuloService.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.dpc.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -22,6 +23,13 @@ public class ArticuloService {
 	@Transactional
 	public void guardarArticulo(Articulo articulo) {
 		articuloRepository.save(articulo);
+	}
+	
+
+	@Transactional
+	public void eliminarArticulo(Integer articuloId) {
+		Optional<Articulo> articulo = articuloRepository.findById(articuloId);
+		articulo.get().setStock(0);
 	}
 
 	@Transactional
