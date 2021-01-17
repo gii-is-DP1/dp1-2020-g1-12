d370f979d8939c0c7cf9da807b2a5cedb64a6cc4
Jesus <jesusbarba11@gmail.com>
2021-01-17 19:27:57 +0100

    Integración de Logs y aplicación en controladores.

--- src/test/java/org/springframework/samples/dpc/service/TarjetaServiceTest.java
+++ src/test/java/org/springframework/samples/dpc/service/TarjetaServiceTest.java
@@ -6,11 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.samples.dpc.model.Cliente;
 import org.springframework.samples.dpc.model.TarjetaCredito;
-import org.springframework.samples.dpc.model.User;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
