30040678e4a1de4bf2893b53a65272f27991a929
dansuaper <16dsp918@gmail.com>
2021-01-31 21:10:56 +0100

    Editar contraseña en moderador

--- src/main/java/org/springframework/samples/dpc/service/ModeradorService.java
+++ src/main/java/org/springframework/samples/dpc/service/ModeradorService.java
@@ -2,7 +2,11 @@ package org.springframework.samples.dpc.service;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.dpc.model.Moderador;
+import org.springframework.samples.dpc.model.User;
 import org.springframework.samples.dpc.repository.ModeradorRepository;
+import org.springframework.samples.dpc.service.exceptions.ContrasenyaNecesariaException;
+import org.springframework.samples.dpc.service.exceptions.ContrasenyaNoCoincideException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,14 +31,26 @@ public class ModeradorService {
 		moderadorRepository.save(moderador);		
 	}
 	
-	@Transactional
-	public void editar(Moderador moderador, Integer moderadorId) {
-		Moderador mod = moderadorRepository.findById(moderadorId).get();
-		mod.setNombre(moderador.getNombre());
-		mod.setApellido(moderador.getApellido());
-		mod.setDni(moderador.getDni());
-		mod.setDireccion(moderador.getDireccion());
-		mod.setTelefono(moderador.getTelefono());
+	@Transactional(rollbackFor = {ContrasenyaNoCoincideException.class, ContrasenyaNecesariaException.class})
+	public void editar(Moderador moderador, Integer id) throws Exception {
+		Moderador moderadorGuardado = findModeradorById(id);
+		User moderadorUser = moderador.getUser();
+		if(moderadorUser.getPassword() != "" && moderadorUser.getUsername() != "") {
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			if(passwordEncoder.matches(moderadorUser.getPassword(), moderadorGuardado.getUser().getPassword())) {
+				String cifrado = new BCryptPasswordEncoder().encode(moderadorUser.getUsername());
+				moderadorGuardado.getUser().setPassword(cifrado);
+			} else {
+				throw new ContrasenyaNoCoincideException();
+			}
+		} else if(moderadorUser.getPassword() == "" && moderadorUser.getUsername() != "") {
+			throw new ContrasenyaNecesariaException();
+		}
+		moderadorGuardado.setNombre(moderador.getNombre());
+		moderadorGuardado.setApellido(moderador.getApellido());
+		moderadorGuardado.setDni(moderador.getDni());
+		moderadorGuardado.setDireccion(moderador.getDireccion());
+		moderadorGuardado.setTelefono(moderador.getTelefono());
 	}
 	
 	@Transactional(readOnly = true)
